f49972d43e3b6db844a637e5ab1590f10838cce9
poula9744 <poula9744@naver.com>
2024-02-21 17:52:43 +0900

    2024-02-21

---
+++ src/main/java/com/javaex/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javaex.dao.UserDao;
+import com.javaex.util.WebUtil;
+import com.javaex.vo.UserVo;
+
+
+@WebServlet("/user")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("UserController");
+		
+		//user에서 업무구분
+		String action = request.getParameter("action");
+		System.out.println(action);
+		
+		if("joinform".equals(action)) {
+			System.out.println("joinform: 등록");
+			
+			//회원가입 폼
+			WebUtil.forward(request, response, "/WEB-INF/views/user/joinForm.jsp");
+		}else if("join".equals(action)) {
+			System.out.println("user>join");
+			
+			//파라미터에서 데이터 꺼내기
+			String id = request.getParameter("id");
+			String password = request.getParameter("password");
+			String name = request.getParameter("name");
+			String gender = request.getParameter("gender");
+			
+			//데이터를 Vo로 묶기
+			UserVo userVo = new UserVo(id, password, name, gender);
+			System.out.println(userVo);
+
+			//dao의 메소드로 회원가입
+			UserDao userDao = new UserDao();
+			userDao.insertUser(userVo);
+			
+			//joinOk.jsp 포워드
+			WebUtil.forward(request, response, "/WEB-INF/views/user/joinOk.jsp");
+			
+		}else {
+			System.out.println("action값을 다시 확인해주세요.");
+		}
+		
+	}
+	
+	
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/com/javaex/vo/UserVo.java
@@ -0,0 +1,85 @@
+package com.javaex.vo;
+
+public class UserVo {
+	
+	//필드
+	private int no;
+	private String id;
+	private String pw;
+	private String name;
+	private String gender;
+	
+	//생성자
+	public UserVo() {
+		super();
+	}
+	
+
+	public UserVo(String id, String pw, String name, String gender) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.gender = gender;
+	}
+
+
+	public UserVo(int no, String id, String pw, String name, String gender) {
+		super();
+		this.no = no;
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.gender = gender;
+	}
+
+	
+	//메소드 - g/s
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	//메소드 - 일반
+	@Override
+	public String toString() {
+		return "UserVo [no=" + no + ", id=" + id + ", pw=" + pw + ", name=" + name + ", gender=" + gender + "]";
+	}
+	
+	
+}
